b3723b61a58787276c5356e7f861bdf805443da2
Stefanos <spalaio@moby-it.com>
2024-12-06 10:40:47 +0200

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+
+export default {
+  content: ["./src/**/*.{astro,svelte,js,jsx,ts,tsx}", "./public/**/*.html"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "hex-blue-400": "#0A1428",
+        "hex-gold-400": "#C89B3C",
+      },
+    },
   },
   plugins: [],
-}
-
+};
