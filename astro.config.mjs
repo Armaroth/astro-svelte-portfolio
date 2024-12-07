8b4e0f205adcb8bc7e4f19d0bc849234bfe766a9
Stefanos <spalaio@moby-it.com>
2024-12-07 18:43:07 +0200

    il8n package
    Installed il8n package for multi language supprt
    Created Language.svelte component to handle language switching
    modified astro.config to hanlde taking the language from the url params

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
+import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  output: "server", // Enable SSR
+  adapter: node({ mode: "standalone" }), // Add the required `mode` option
 });
